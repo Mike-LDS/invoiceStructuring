259be9b2ec72d53c2e257d6e26d1268bb6a33b48
Mike-LDS <81445082+Mike-LDS@users.noreply.github.com>
2022-05-20 12:52:16 -0700

    update

--- invoiceStructuring.py
+++ invoiceStructuring.py
@@ -62,29 +62,55 @@ while True:
             with open(values['-INVOICES-'], newline='', encoding="utf-8") as csvfile:
                 reader = csv.DictReader(csvfile)
                 for row in reader:
+                    if 'explicit' in row['Description'].lower():
+                        item = 'Explicit Instruction'
+                    elif 'support' in row['Description'].lower():
+                        item = 'Remedial & Homework Support'
+                    elif 'rise at school' in row['Description'].lower():
+                        item = 'RISE AT School'
+                    elif 'slp' in row['Description'].lower():
+                        item = 'SLP'
+                    elif 'rise now' in row['Description'].lower():
+                        item = 'RISE Now'
+                    elif 'rise team' in row['Description'].lower():
+                        item = 'RISE Team'
+                    elif 'ktea-3' in row['Description'].lower():
+                        item = 'KTEA-3 Assessment'
+                    elif 'social language group' in row['Description'].lower():
+                        item = 'Social Language Group'
+                    elif 'summer tutoring' in row['Description'].lower():
+                        item = 'Summer Tutoring'
+                    elif 'summer rise' in row['Description'].lower():
+                        item = 'Summer RISE'
+                    elif 'intensive' in row['Description'].lower():
+                        item = 'Summer RISE'
+                    elif 'summer camp' in row['Description'].lower():
+                        item = 'Summer Camp'
+                    elif 'spring break camp' in row['Description'].lower():
+                        item = 'Spring Break Camp'
+                    elif 'early risers' in row['Description'].lower():
+                        item = 'Early RISErs'
+                    elif 'intake' in row['Description'].lower():
+                        item = 'Intake and Assessment Fee'
+                    elif 'onboarding' in row['Description'].lower():
+                        item = 'Intake and Assessment Fee'
+                    elif 'social language' in row['Description'].lower():
+                        item = 'SLP'
+                    elif 'family coaching' in row['Description'].lower():
+                        item = 'Family Coaching'
+                    elif 'p.e.e.r.s' in row['Description'].lower():
+                        item = 'P.E.E.R.S'
+                    elif 'homework club' in row['Description'].lower():
+                        item = 'Homework Club'
+                    elif 'neuropsychological' in row['Description'].lower():
+                        item = 'Neuropsychological Assessment Fees'
+                    elif 'parent workshop' in row['Description'].lower():
+                        item = 'Parent Workshop'
+                    elif 'wamms' in row['Description'].lower():
+                        item = 'WAMMS'
+                    else:
+                        item = 'ERROR'
                     if row['TrackingOption1'] == "lesson":
-                        if 'Explicit' in row['Description']:
-                            item = 'Explicit Instruction'
-                        elif 'Support' in row['Description']:
-                            item = 'Remedial & Homework Support'
-                        elif 'RISE AT School' in row['Description']:
-                            item = 'RISE AT School'
-                        elif 'SLP' in row['Description']:
-                            item = 'SLP'
-                        elif 'RISE Now' in row['Description']:
-                            item = 'RISE Now'
-                        elif 'RISE Team' in row['Description']:
-                            item = 'RISE Team'
-                        elif 'KTEA-3' in row['Description']:
-                            item = 'KTEA-3 Assessment'
-                        elif 'KTEA-3' in row['Description']:
-                            item = 'KTEA-3 Assessment'
-                        elif 'Social Language Group' in row['Description']:
-                            item = 'Social Language Group'
-                        elif 'Summer Tutoring' in row['Description']:
-                            item = 'Summer Tutoring'    
-                        else:
-                            item = 'ERROR'
                         cost = float(row['Quantity'])*float(row['UnitAmount'])
                         for client in invoice_2_client:
                             if row['InvoiceNumber'] in client[1]:
@@ -99,36 +125,18 @@ while True:
                                                 invoice_detialed.append(line)
                                                 match = True
                                 if match == False:
+                                    for student in client_2_students:
+                                        if client_ID == student[2]:
+                                            customer = student[0]
+                                            line = [row['InvoiceNumber'], customer, client[2], client[3], item, row['Quantity'], row['UnitAmount'], cost,'E','Exclusive',row['StartDate']]
+                                            invoice_detialed.append(line)
+                                            match = True
+                                            break
+                                if match == False:
                                     line = [row['InvoiceNumber'], client_ID, client[2], client[3], item, row['Quantity'], row['UnitAmount'], cost,'E','Exclusive',row['StartDate']]
                                     invoice_detialed.append(line)
 
                     elif row['TrackingOption1'] == "adhoc_charge":
-                        if 'Intensive' in row['Description']:
-                            item = 'Intensive Intervention'
-                        elif 'Summer Camp' in row['Description']:
-                            item = 'Summer Camp'
-                        elif 'Spring Break Camp' in row['Description']:
-                            item = 'Spring Break Camp'
-                        elif 'Early RISErs' in row['Description']:
-                            item = 'Early RISErs'
-                        elif 'Intake' in row['Description']:
-                            item = 'Intake and Onboarding Fee'
-                        elif 'Intake' in row['Description']:
-                            item = 'Intake and Onboarding Fee'
-                        elif 'Onboarding' in row['Description']:
-                            item = 'Intake and Onboarding Fee'
-                        elif 'SLP' in row['Description']:
-                            item = 'SLP'
-                        elif 'Social Language' in row['Description']:
-                            item = 'SLP'
-                        elif 'KTEA-3' in row['Description']:
-                            item = 'KTEA-3 Assessment'
-                        elif 'Family Coaching' in row['Description']:
-                            item = 'Family Coaching'
-                        elif 'P.E.E.R.S' in row['Description']:
-                            item = 'P.E.E.R.S'
-                        else:
-                            item = 'ERROR'
                         for client in invoice_2_client:
                             if row['InvoiceNumber'] in client[1]:
                                 client_ID = client[0]
@@ -174,6 +182,9 @@ while True:
                     row[header_s.index('Item')] = 'SLP'
                 elif row[header_s.index('Item')] == 'Summer Tutoring':
                     row[header_s.index('Item')] = 'Remedial & Homework Support'
+            for row in adhoc:
+                if row[header_s.index('Item')] == 'Summer Camp' or row[header_s.index('Item')] == 'Spring Break Camp':
+                    row[header_s.index('Item')] = 'CAMPS'
 
         # Adding Invoice Number:
             for row in invoice_sum:
